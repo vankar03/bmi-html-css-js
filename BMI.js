f7a51f5deb549dd3e8ee02f8ea11eaf09d4db33a
vankar03 <128495657+vankar03@users.noreply.github.com>
2023-09-11 07:40:43 +0530

    Add files via upload

---
+++ BMI.js
@@ -0,0 +1,48 @@
+var weight, height, measure, bmi, error ;
+
+function calculate() {
+	weight = document.getElementById("weight").value;
+	height = document.getElementById("height").value;
+	error = "Please enter some values";
+	height /= 100;
+	height *= height;
+	bmi = weight/height;
+	bmi = bmi.toFixed(1);
+
+	if (bmi <= 18.4) {
+		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
+	} else if (bmi >= 25 && bmi <= 29.9) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+	} else if (bmi >= 30) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+	}
+	
+          // to get the output text
+	if (weight === 0 ) {
+		document.getElementById("results").innerHTML = error;
+	} else if (height === 0){
+		document.getElementById("results").innerHTML = error;
+	}
+	 else {
+
+		document.getElementById("results").innerHTML = measure;
+	}
+	if (weight < 0) {
+		document.getElementById("results").innerHTML = "Negative Values not Allowed";
+	}
+
+
+                    // to get the output text
+	if (bmi<=18.4) {
+		document.getElementById("normal").innerHTML = "to normalize your bmi";
+	} else if (bmi>=18.5 && bmi<=24.9){
+		document.getElementById("normal").innerHTML = "you are in perfect condition";
+	}
+	 else {
+
+		document.getElementById("normal").innerHTML = "to decrease your bmi and normalize it";
+	}
+	
+}
